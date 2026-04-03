e443321fb1e01cd826917624e54977cdacba7015
Frank Paul Silye <frankps@gmail.com>
2026-04-03 07:28:17 +0200

    Add files via upload

---
+++ translations/harbour-camera-buddy-no_NB.ts
@@ -0,0 +1,297 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="no_NB">
+<context>
+    <name>CamerasPage</name>
+    <message>
+        <location filename="../qml/pages/CamerasPage.qml" line="47"/>
+        <source>Refresh</source>
+        <translation>Oppdater</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CamerasPage.qml" line="43"/>
+        <source>Settings</source>
+        <translation>Innstillinger</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CamerasPage.qml" line="62"/>
+        <source>Connected Cameras</source>
+        <translation>Tilkoblede kameraer</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CamerasPage.qml" line="92"/>
+        <location filename="../qml/pages/CamerasPage.qml" line="155"/>
+        <source>Unknown Camera</source>
+        <translation>Ukjent kamera</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CamerasPage.qml" line="100"/>
+        <source>Model: %1</source>
+        <translation>Modell: %1</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CamerasPage.qml" line="100"/>
+        <location filename="../qml/pages/CamerasPage.qml" line="109"/>
+        <location filename="../qml/pages/CamerasPage.qml" line="118"/>
+        <source>Unknown</source>
+        <translation>Ukjent</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CamerasPage.qml" line="109"/>
+        <source>Port: %1</source>
+        <translation>Port: %1</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CamerasPage.qml" line="118"/>
+        <source>Serial: %1</source>
+        <translation>Serial: %1</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CamerasPage.qml" line="136"/>
+        <source>Connected</source>
+        <translation>Tilkoblet</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CamerasPage.qml" line="136"/>
+        <source>Disconnected</source>
+        <translation>Frakoblet</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CamerasPage.qml" line="164"/>
+        <source>No cameras found</source>
+        <translation>Ingen kamera funnet</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CamerasPage.qml" line="165"/>
+        <source>Make sure your camera is turned on and connected via USB or via WiFi. Pull down to refresh or add PTP/IP camera manually.</source>
+        <translation>Sørg for at kameraet er slått på og tilkoblet via USB eller Wi-Fi. Dra ned for å oppdatere, eller legg til PTP/IP-kamera manuelt.</translation>
+    </message>
+</context>
+<context>
+    <name>Cover</name>
+    <message>
+        <location filename="../qml/pages/Cover.qml" line="36"/>
+        <source>Camera
+Buddy</source>
+        <translation>Camera
+Buddy</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Cover.qml" line="57"/>
+        <source>Downloading...</source>
+        <translation>Laster ned...</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Cover.qml" line="65"/>
+        <source>%1 / %2</source>
+        <translation>%1 / %2</translation>
+    </message>
+</context>
+<context>
+    <name>DownloadModel</name>
+    <message>
+        <location filename="../src/DownloadModel.cpp" line="66"/>
+        <source>File already exists</source>
+        <translation>Filen eksisterer allerede</translation>
+    </message>
+    <message>
+        <location filename="../src/DownloadModel.cpp" line="76"/>
+        <source>Failed to create file: %1</source>
+        <translation>Kunne ikke opprette filen: %1</translation>
+    </message>
+    <message>
+        <location filename="../src/DownloadModel.cpp" line="92"/>
+        <source>Failed to download photo data</source>
+        <translation>Kunne ikke laste ned bildedata</translation>
+    </message>
+    <message>
+        <location filename="../src/DownloadModel.cpp" line="107"/>
+        <source>Exception: %1</source>
+        <translation>Unntak: %1</translation>
+    </message>
+</context>
+<context>
+    <name>DownloadPage</name>
+    <message>
+        <location filename="../qml/pages/DownloadPage.qml" line="87"/>
+        <source>Downloading Photos</source>
+        <translation>Laster ned bilder</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DownloadPage.qml" line="165"/>
+        <source>Pending...</source>
+        <translation>Venter..</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DownloadPage.qml" line="166"/>
+        <source>Downloading... %1%</source>
+        <translation>Laster ned... %1%</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DownloadPage.qml" line="169"/>
+        <source>Completed: %1</source>
+        <translation>Fullført: %1</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DownloadPage.qml" line="171"/>
+        <source>Completed</source>
+        <translation>Fullført</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DownloadPage.qml" line="173"/>
+        <source>Error: %1</source>
+        <translation>Feil: %1</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DownloadPage.qml" line="196"/>
+        <source>No downloads</source>
+        <translation>Ingen nedlastinger</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DownloadPage.qml" line="197"/>
+        <source>No photos selected for download</source>
+        <translation>Ingen bilder er valgt for nedlasting</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DownloadPage.qml" line="216"/>
+        <source>Overall Progress: %1 / %2</source>
+        <translation>Samlet fremdrift: %1 / %2</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <source>Settings</source>
+        <translation>Innstillinger</translation>
+    </message>
+    <message>
+        <source>Camera Buddy</source>
+        <translation type="vanished">Camera Buddy</translation>
+    </message>
+    <message>
+        <source>Welcome to Camera Buddy!</source>
+        <translation type="vanished">Velkommen til Camera Buddy!</translation>
+    </message>
+    <message>
+        <source>Detect Cameras</source>
+        <translation>Søk etter kameraer</translation>
+    </message>
+    <message>
+        <source>Connected Cameras</source>
+        <translation>Tilkoblede kameraer</translation>
+    </message>
+    <message>
+        <source>No cameras detected</source>
+        <translation>Ingen kameraer funnet</translation>
+    </message>
+</context>
+<context>
+    <name>NewFolderDialog</name>
+    <message>
+        <location filename="../qml/dialogs/NewFolderDialog.qml" line="39"/>
+        <source>Create</source>
+        <translation>Opprett</translation>
+    </message>
+    <message>
+        <location filename="../qml/dialogs/NewFolderDialog.qml" line="45"/>
+        <location filename="../qml/dialogs/NewFolderDialog.qml" line="46"/>
+        <source>Folder name</source>
+        <translation>Mappenavn</translation>
+    </message>
+</context>
+<context>
+    <name>PhotosPage</name>
+    <message>
+        <location filename="../qml/pages/PhotosPage.qml" line="55"/>
+        <source>Download to</source>
+        <translation>Last ned til</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/PhotosPage.qml" line="134"/>
+        <source>Photos</source>
+        <translation>Bilder</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/PhotosPage.qml" line="139"/>
+        <source>Refresh</source>
+        <translation>Oppdater</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/PhotosPage.qml" line="151"/>
+        <source>Select All</source>
+        <translation>Velg alle</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/PhotosPage.qml" line="147"/>
+        <source>Select JPEGs</source>
+        <translation>Velg bilder (JPEG)</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/PhotosPage.qml" line="143"/>
+        <source>Deselect All</source>
+        <translation>Fjern alle valg</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/PhotosPage.qml" line="155"/>
+        <location filename="../qml/pages/PhotosPage.qml" line="164"/>
+        <source>Download Selected</source>
+        <translation>Last ned utvalg</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/PhotosPage.qml" line="258"/>
+        <source>No photos found</source>
+        <translation>Ingen bilder funnet</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/PhotosPage.qml" line="259"/>
+        <source>Pull down to refresh</source>
+        <translation>Trekk ned for å oppdatere</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../src/PhotosModel.cpp" line="225"/>
+        <source>Camera not available or not connected</source>
+        <translation type="unfinished">Kameraet er ikke tilgjengelig eller tilkoblet</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="45"/>
+        <source>Settings</source>
+        <translation>Innstillinger</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="46"/>
+        <source>Save</source>
+        <translation>Lagre</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="47"/>
+        <source>Cancel</source>
+        <translation>Avbryt</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="51"/>
+        <source>PTP/IP Camera</source>
+        <translation>PTP/IP-kamera</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="57"/>
+        <source>IP Address</source>
+        <translation>IP-adresse</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="58"/>
+        <source>Enter IP address (e.g., 192.168.1.1)</source>
+        <translation>Skriv inn IP-adresse</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="76"/>
+        <source>Enter the IP address of your PTP/IP compatible camera. The camera will be detected automatically after you accept the settings.</source>
+        <translation>Skriv inn IP-adressen til ditt PTP/IP-kompatible kamera. Kameraet blir funnet automatisk etter at du har godkjent innstillingene.</translation>
+    </message>
+</context>
+</TS>
